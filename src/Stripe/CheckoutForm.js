db62fdd696ba48ceba75905ffce8b4f6e35f4b40
Grazhinka <gr-km@mail.ru>
2022-12-04 19:18:22 +0300

    edit pay text

--- src/Stripe/CheckoutForm.js
+++ src/Stripe/CheckoutForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from "axios";
-import { getTotals } from "../Redux/cartSlice";
 
 
 export const CheckoutForm = ({sumWithSale}) => {
