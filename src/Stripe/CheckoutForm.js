f08e0400c4a0e7119bfd279e70a4f57a17b02838
Grazhinka <gr-km@mail.ru>
2022-12-04 19:29:09 +0300

    change link

--- src/Stripe/CheckoutForm.js
+++ src/Stripe/CheckoutForm.js
@@ -21,7 +21,7 @@ export const CheckoutForm = ({sumWithSale}) => {
       try {
         const { id } = paymentMethod;
         const response = await axios.post(
-          "http://shop-tk.netlify.app/shop:8080/stripe/charge",
+          "http://localhost:8080/stripe/charge",
           {
             amount: sumWithSale*100,
             id: id,
